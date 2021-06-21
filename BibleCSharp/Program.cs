001156a55f0f841bccace1fca691c75cb78ae570
Alberto Chirinos <developernet@outlook.com>
2021-06-21 16:11:48 -0500

    Add project files.

---
+++ BibleCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BibleCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, enter your name\n");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("\n" + SimpleString.Welcome(name));
+
+            Console.WriteLine("\n\nPress a key, please");
+
+            Console.ReadKey();
+
+            Console.WriteLine("\n-------------------------\nGoodbye!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BibleCSharp/SimpleString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibleCSharp
+{
+    public static class SimpleString
+    {
+        public static string Welcome(string name)
+        {
+            return "Welcome " + name + "!";
+        }
+    }
+}
